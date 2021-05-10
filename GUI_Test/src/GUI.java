2531629d64d07ee87ebc9dd820718734c8ddb61a
Alx-Lai <b09902135@csie.ntu.edu.tw>
2021-05-10 13:50:53 +0800

    add new windows

--- GUI_Test/src/GUI.java
+++ GUI_Test/src/GUI.java
@@ -17,8 +17,21 @@ public class GUI {
 	private String[] TripType = { "單程", "去回程" };
 	private String[] TicketType = { "無", "早鳥", "大學生" };
 	private String[] SeatType = {"標準車廂", "商務車廂"};
-	private String[] TicketCount= {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+	private String[] TicketCountChoice= {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+	private int[][] carQueryResult = //車次, 開始時, 開始分, 結束時, 結束分, 行車時, 行車分, 折數
+		{
+				{838,17,32,17,39,0,7,100},
+				{242,17,47,17,54,0,7,80},
+				{660,17,51,17,59,0,8,65}
+		};
+	private String startStation = "南港";
+	private String endStation = "南港";
+	private Date tripDate;
+	private int checked = 0; 
 	private int mode = 1;
+	private int ticketPrice = 1350;
+	private String mySeatType = "標準車廂";
+	private int[] ticketCount = {1, 0, 1}; //全票, 早鳥, 大學生
 	private Calendar nowDate;
 	private int year,month,date;
 
@@ -49,6 +62,7 @@ public class GUI {
 		/*time = new Date();
 		nowDate.setTime(time);
 		*/
+		tripDate = new Date();
 		nowDate = Calendar.getInstance();
 		year = nowDate.get(Calendar.YEAR);
 		month = nowDate.get(Calendar.MONTH);
@@ -195,8 +209,7 @@ public class GUI {
 		panel2.add(endStationComboBox2, getConstraints(1, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
 		panel2.add(tripTypeComboBox2, getConstraints(2, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
 		panel2.add(goDatePicker2, getConstraints(3, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
-		panel2.add(goTimeSpinner2, getConstraints(4, 1, 2, 1, 0, 0, GridBagConstraints.BOTH));
-		
+		panel2.add(goTimeSpinner2, getConstraints(4, 1, 2, 1, 0, 0, GridBagConstraints.BOTH));		
 		JLabel backDateLabel2 = new JLabel("回程日期");
 		JLabel backTimeLabel2 = new JLabel("回程時間");
 		UtilDateModel backModel2 = new UtilDateModel();
@@ -236,11 +249,11 @@ public class GUI {
 		panel2.add(Label2_11, getConstraints(5, 4, 1, 1, 0, 0, GridBagConstraints.BOTH));
 		
 		JComboBox seatTypeComboBox = new JComboBox(SeatType);
-		JComboBox adultCountComboBox = new JComboBox(TicketCount);
-		JComboBox kidCountComboBox = new JComboBox(TicketCount);
-		JComboBox heartCountComboBox = new JComboBox(TicketCount);
-		JComboBox elderCountComboBox = new JComboBox(TicketCount);
-		JComboBox collegeCountComboBox = new JComboBox(TicketCount);
+		JComboBox adultCountComboBox = new JComboBox(TicketCountChoice);
+		JComboBox kidCountComboBox = new JComboBox(TicketCountChoice);
+		JComboBox heartCountComboBox = new JComboBox(TicketCountChoice);
+		JComboBox elderCountComboBox = new JComboBox(TicketCountChoice);
+		JComboBox collegeCountComboBox = new JComboBox(TicketCountChoice);
 		
 
 		panel2.add(seatTypeComboBox, getConstraints(0, 5, 1, 1, 0, 0, GridBagConstraints.BOTH));
@@ -254,6 +267,204 @@ public class GUI {
 		queryButton2.setBackground(Color.ORANGE);
 		
 		panel2.add(queryButton2, getConstraints(0, 6, 6, 1, 0, 0, GridBagConstraints.BOTH));
+		
+		
+		
+		JPanel panel3 = new JPanel(new GridBagLayout());
+		JLabel label3_0 = new JLabel("去程："+startStation+" - "+ endStation + "  "+ String.format("%02d", tripDate.getMonth()+1)+"/"+String.format("%02d", tripDate.getDate()));
+		JLabel label3_1 = new JLabel("選擇");
+		JLabel label3_2 = new JLabel("車次");
+		JLabel label3_3 = new JLabel("全票優惠*");
+		JLabel label3_4 = new JLabel("出發時間");
+		JLabel label3_5 = new JLabel("到達時間");
+		JLabel label3_6 = new JLabel("行車時間");
+		JLabel label3_7 = new JLabel("訂位明細");
+		label3_0.setForeground(Color.ORANGE);
+		label3_7.setForeground(Color.ORANGE);
+		
+		panel3.add(label3_0, getConstraints(0, 0, 6, 2, 0, 0, GridBagConstraints.NORTHWEST));
+		panel3.add(label3_1, getConstraints(0, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_2, getConstraints(1, 2, 1, 1, 0, 0, GridBagConstraints.EAST));
+		panel3.add(label3_3, getConstraints(2, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_4, getConstraints(3, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_5, getConstraints(4, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_6, getConstraints(5, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		
+		JCheckBox[] checkboxList = new JCheckBox[100];
+		JLabel[] carNumberList = new JLabel[100];
+		JLabel[] discountList = new JLabel[100];
+		JLabel[] goTimeList = new JLabel[100];
+		JLabel[] arriveTimeList = new JLabel[100];
+		JLabel[] totalTimeList = new JLabel[100];
+		
+		/*
+		 * 車次	全票優惠*	出發時間	到達時間	行車時間
+		 */
+		for(int i=0;i<carQueryResult.length;i++) {
+			checkboxList[i] = new JCheckBox();
+			carNumberList[i] = new JLabel(carQueryResult[i][0]+"");
+			goTimeList[i] = new JLabel(String.format("%02d",carQueryResult[i][1])+":"+String.format("%02d",carQueryResult[i][2]));
+			arriveTimeList[i] = new JLabel(String.format("%02d",carQueryResult[i][3])+":"+String.format("%02d",carQueryResult[i][4]));
+			totalTimeList[i] = new JLabel(String.format("%02d",carQueryResult[i][5])+":"+String.format("%02d",carQueryResult[i][6]));
+			if(carQueryResult[i][7] == 100) {
+				discountList[i] = new JLabel();
+			}else {
+				discountList[i] = new JLabel(carQueryResult[i][7]+"折");
+			}
+
+			panel3.add(checkboxList[i], getConstraints(0, i+3, 1, 1, 0, 0, GridBagConstraints.EAST));
+			panel3.add(carNumberList[i], getConstraints(1, i+3, 1, 1, 0, 0, GridBagConstraints.EAST));
+			panel3.add(discountList[i], getConstraints(2, i+3, 1, 1, 0, 0, GridBagConstraints.EAST));
+			panel3.add(goTimeList[i], getConstraints(3, i+3, 1, 1, 0, 0, GridBagConstraints.EAST));
+			panel3.add(arriveTimeList[i], getConstraints(4, i+3, 1, 1, 0, 0, GridBagConstraints.EAST));
+			panel3.add(totalTimeList[i], getConstraints(5, i+3, 1, 1, 0, 0, GridBagConstraints.EAST));
+		}
+		checkboxList[0].setSelected(true);
+		
+
+		panel3.add(label3_7, getConstraints(0, carQueryResult.length+3, 6, 2, 0, 0, GridBagConstraints.SOUTHWEST));
+		//行程	日期	車次	起程站	到達站	出發時間	到達時間
+		
+		JLabel label3_8 = new JLabel("行程");
+		JLabel label3_9 = new JLabel("日期");
+		JLabel label3_10 = new JLabel("車次");
+		JLabel label3_11 = new JLabel("起程站");
+		JLabel label3_12 = new JLabel("到達站");
+		JLabel label3_13 = new JLabel("出發時間");
+		JLabel label3_14 = new JLabel("到達時間");
+		
+		panel3.add(label3_8, getConstraints(0, carQueryResult.length+5, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_9, getConstraints(1, carQueryResult.length+5, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_10, getConstraints(2, carQueryResult.length+5, 1, 1, 0, 0, GridBagConstraints.EAST));
+		panel3.add(label3_11, getConstraints(3, carQueryResult.length+5, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_12, getConstraints(4, carQueryResult.length+5, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_13, getConstraints(5, carQueryResult.length+5, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_14, getConstraints(6, carQueryResult.length+5, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		
+		JLabel label3_15 = new JLabel("去程");
+		JLabel label3_16 = new JLabel(String.format("%02d", tripDate.getMonth()+1)+"/"+String.format("%02d", tripDate.getDate()));
+		JLabel label3_17 = new JLabel(""+carQueryResult[checked][0]);
+		JLabel label3_18 = new JLabel(""+startStation);
+		JLabel label3_19 = new JLabel(""+endStation);
+		JLabel label3_20 = new JLabel(String.format("%02d",carQueryResult[checked][1])+":"+String.format("%02d",carQueryResult[checked][2]));
+		JLabel label3_21 = new JLabel(String.format("%02d",carQueryResult[checked][3])+":"+String.format("%02d",carQueryResult[checked][4]));
+		
+		panel3.add(label3_15, getConstraints(0, carQueryResult.length+6, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_16, getConstraints(1, carQueryResult.length+6, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_17, getConstraints(2, carQueryResult.length+6, 1, 1, 0, 0, GridBagConstraints.EAST));
+		panel3.add(label3_18, getConstraints(3, carQueryResult.length+6, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_19, getConstraints(4, carQueryResult.length+6, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_20, getConstraints(5, carQueryResult.length+6, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_21, getConstraints(6, carQueryResult.length+6, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		
+		String tmp = "";
+		if(ticketCount[0] > 0) {
+			tmp += "全票"+ticketCount[0]+"張";
+		}
+		if(ticketCount[1] > 0) {
+			tmp += " 早鳥"+ticketCount[1]+"張";
+		}
+		if(ticketCount[2] > 0) {
+			tmp += " 大學生"+ticketCount[2]+"張";
+		}
+		
+		JLabel label3_22 = new JLabel(mySeatType);
+		JLabel label3_23 = new JLabel(tmp);
+		
+		panel3.add(label3_22, getConstraints(0, carQueryResult.length+7, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(label3_23, getConstraints(1, carQueryResult.length+7, 1, 1, 0, 0, GridBagConstraints.BOTH));
+
+		JButton button3_1 = new JButton("重新查詢");
+		JButton button3_2 = new JButton("確認車次");
+		button3_1.setBackground(new Color(153,153,153));
+		button3_2.setBackground(Color.ORANGE);
+		
+		panel3.add(button3_1, getConstraints(0, carQueryResult.length+8, 2, 1, 0, 0, GridBagConstraints.BOTH));
+		panel3.add(button3_2, getConstraints(5, carQueryResult.length+8, 2, 1, 0, 0, GridBagConstraints.BOTH));
+		
+		
+		JPanel panel4 = new JPanel(new GridBagLayout());
+
+		JLabel label4_1 = new JLabel("訂位明細");
+		JLabel label4_2 = new JLabel("行程");
+		JLabel label4_3 = new JLabel("日期");
+		JLabel label4_4 = new JLabel("車次");
+		JLabel label4_5 = new JLabel("起程站");
+		JLabel label4_6 = new JLabel("到達站");
+		JLabel label4_7 = new JLabel("出發時間");
+		JLabel label4_8 = new JLabel("到達時間");
+		JLabel label4_9 = new JLabel("全票");
+		JLabel label4_10 = new JLabel("敬老、孩童、愛心");
+		JLabel label4_11 = new JLabel("小計");
+		label4_1.setForeground(Color.ORANGE);
+		
+		panel4.add(label4_1, getConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_2, getConstraints(0, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_3, getConstraints(1, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_4, getConstraints(2, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_5, getConstraints(3, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_6, getConstraints(4, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_7, getConstraints(5, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_8, getConstraints(6, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_9, getConstraints(7, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_10, getConstraints(8, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_11, getConstraints(9, 1, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		
+		JLabel label4_12 = new JLabel("去程");
+		JLabel label4_13 = new JLabel(String.format("%02d", tripDate.getMonth()+1)+"/"+String.format("%02d", tripDate.getDate()));
+		JLabel label4_14 = new JLabel(""+carQueryResult[checked][0]);//車次, 開始時, 開始分, 結束時, 結束分, 行車時, 行車分, 折數
+		JLabel label4_15 = new JLabel(""+startStation);
+		JLabel label4_16 = new JLabel(""+endStation);
+		JLabel label4_17 = new JLabel(String.format("%02d",carQueryResult[checked][1])+":"+String.format("%02d",carQueryResult[checked][2]));
+		JLabel label4_18 = new JLabel(String.format("%02d",carQueryResult[checked][3])+":"+String.format("%02d",carQueryResult[checked][4]));
+		JLabel label4_19 = new JLabel(""+ticketCount[0]);
+		JLabel label4_20 = new JLabel(""+ticketCount[1]+", "+ticketCount[2]);
+		JLabel label4_21 = new JLabel("TWD"+ticketPrice);
+		
+		panel4.add(label4_12, getConstraints(0, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_13, getConstraints(1, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_14, getConstraints(2, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_15, getConstraints(3, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_16, getConstraints(4, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_17, getConstraints(5, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_18, getConstraints(6, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_19, getConstraints(7, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_20, getConstraints(8, 2, 1, 1, 0, 0, GridBagConstraints.EAST));
+		panel4.add(label4_21, getConstraints(9, 2, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		
+
+		JLabel label4_22 = new JLabel("車廂: "+mySeatType);
+		tmp = "";
+		if(ticketCount[0] > 0) {
+			tmp += "全票"+ticketCount[0]+"張";
+		}
+		if(ticketCount[1] > 0) {
+			tmp += " 早鳥"+ticketCount[1]+"張";
+		}
+		if(ticketCount[2] > 0) {
+			tmp += " 大學生"+ticketCount[2]+"張";
+		}
+		JLabel label4_23 = new JLabel("票數: "+ tmp);
+		JLabel label4_24 = new JLabel("總票價 TWD" + ticketPrice);
+		label4_24.setForeground(Color.RED);
+		panel4.add(label4_22, getConstraints(0, 3, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_23, getConstraints(1, 3, 1, 1, 0, 0, GridBagConstraints.EAST));
+		panel4.add(label4_24, getConstraints(9, 3, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		
+		JLabel label4_25 = new JLabel("取票人資訊");
+		JLabel label4_26 = new JLabel("身分證號碼");
+		JTextArea textarea4_1 = new JTextArea();
+		label4_25.setForeground(Color.ORANGE);
+		panel4.add(label4_25, getConstraints(0, 4, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(label4_26, getConstraints(0, 5, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(textarea4_1, getConstraints(1, 5, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		
+		JButton button4_1 = new JButton("取消");
+		JButton button4_2 = new JButton("完成訂位");
+		button4_2.setBackground(Color.ORANGE);
+		panel4.add(button4_1, getConstraints(0, 6, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		panel4.add(button4_2, getConstraints(9, 6, 1, 1, 0, 0, GridBagConstraints.BOTH));
+		
 		
 		backDateLabel.setVisible(false);
 		backDatePicker.setVisible(false);
@@ -310,6 +521,7 @@ public class GUI {
 				mode = 1;
 				panel1.setVisible(true);
 				panel2.setVisible(false);
+				panel3.setVisible(false);
 			}
 		});
 		
@@ -319,6 +531,7 @@ public class GUI {
 				mode = 2;
 				panel1.setVisible(false);
 				panel2.setVisible(true);
+				panel3.setVisible(false);
 			}
 		});
 		
@@ -326,6 +539,9 @@ public class GUI {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				mode = 3;
+				panel1.setVisible(false);
+				panel2.setVisible(false);
+				panel3.setVisible(true);
 			}
 		});
 		
@@ -335,12 +551,42 @@ public class GUI {
 				mode = 4;
 			}
 		});
+		
+		for(int i=0;i<carQueryResult.length;i++) {
+			checkboxList[i].addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					for(int j=0;j<carQueryResult.length;j++) {
+						if(e.getSource() == checkboxList[j]) {
+							checkboxList[j].setSelected(true);
+							checked = j;
+							label3_16.setText(String.format("%02d", tripDate.getMonth()+1)+"/"+String.format("%02d", tripDate.getDate()));
+							label3_17.setText(""+carQueryResult[checked][0]);
+							label3_20.setText(String.format("%02d",carQueryResult[checked][1])+":"+String.format("%02d",carQueryResult[checked][2]));
+							label3_21.setText(String.format("%02d",carQueryResult[checked][3])+":"+String.format("%02d",carQueryResult[checked][4]));
+							
+							break;
+						}
+					}
+					for(int j=0;j<carQueryResult.length;j++) {
+						if(j != checked) {
+							checkboxList[j].setSelected(false);
+						}
+					}
+				}
+			});
+		}
+		
 		//=====================
 		panel1.setVisible(false);
-		panel2.setVisible(true);
+		panel2.setVisible(false);
+		panel3.setVisible(false);
+		panel4.setVisible(true);
 		
 		frame.add(panel1, getConstraints(0, 1, 6, 10, 0, 0, GridBagConstraints.BOTH));
 		frame.add(panel2, getConstraints(0, 1, 6, 10, 0, 0, GridBagConstraints.BOTH));
+		frame.add(panel3, getConstraints(0, 1, 6, 10, 0, 0, GridBagConstraints.BOTH));
+		frame.add(panel4, getConstraints(0, 1, 6, 10, 0, 0, GridBagConstraints.BOTH));
 
 		frame.setVisible(true);
 	}
